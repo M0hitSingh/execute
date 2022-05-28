5027e1160d4ab2e01b70276446291ccd0ca237a1
golchi <golchigeek@gmail.com>
2022-05-29 01:40:18 +0530

    bug fix

--- index.js
+++ index.js
@@ -3,22 +3,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
 const storeroutes = require('./routes/store');
-const passport = require("passport");
-const cookieSession = require('cookie-session');
-require('./passport-setup');
-const proxy = require('express-http-proxy');
+
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cookieSession({
-  name:'tuto-session',
-  keys:['key1','key2']
-}))
+
 app.use(express.urlencoded({extended:true}));
-app.use(passport.initialize());
-app.use(passport.session());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE");
@@ -26,14 +19,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use('/auth',authRoutes);
-app.get("/failed",(req,res)=>res.json("failed to login"));
-app.get('/google',passport.authenticate('google',{scope:['profile','email']}));
-app.get('/auth/google/callback',passport.authenticate("google",{failureRedirect:'/failed'}),
-  function(req,res){
-    res.json("welcome"); 
-  }
-);
 
 
 
